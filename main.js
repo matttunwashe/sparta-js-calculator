fa3b1229d57cfc56f153ead2493873ad128ba0e2
Tech-A20 <Tech-A20@Tech-A20s-MacBook-Air.local>
2017-10-31 07:41:56 +0000

    I managed to finish the calculator a and b but not the bonus tasks

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var typeOfCalculator = prompt("Enter a for an advance calculator and b for a basic calculator");
+var typeOfCalculator = prompt("Please enter a for an advance or b for a basic calculator");
 
 var num1 = parseFloat(prompt("First number?: "));
 
@@ -12,8 +12,9 @@ if(typeOfCalculator == "a") {
   console.log("Invalid option.");
 }
 
-if(operation == "+" || operation == "-" || operation == "*" || operation == "/" || operation == "pow") {
+if(operation == "+" || operation == "-" || operation == "*" || operation == "/" || operation == "pow" || operation == "sqr" ) {
   var num2 = parseFloat(prompt("Second number?: "));
+}
 
 switch (operation) {
   case "+":
@@ -28,10 +29,10 @@ switch (operation) {
   case "/":
     answer = num1 / num2;
     break;
-  case "^":
+  case "pow":
     answer = Math.pow(num1, num2);
     break;
-  case "sqrt":
+  case "sqr":
     answer = Math.sqrt(num1);
     break;
   default:
@@ -39,27 +40,3 @@ switch (operation) {
 }
 
 alert(num1 + " " + operation + " " + num2 + " = " + answer);
-
-
-
-
-
-
-
-// var calc = prompt ('what calculator would you like? (a)dvanced (b)basic (c)BMI')
-// if calc = a || b || c
-// var basic = prompt ('what number do you want to calculate?')
-// var advanced = prompt ('first number you want to calculator?')
-// var BMI = prompt('what is your BMI?')
-//
-// if (basic) {
-//
-// }
-
-// switch (basic) {
-//   case :
-//
-//     break;
-//   default:
-//
-// }
